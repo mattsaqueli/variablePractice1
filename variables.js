8cbddc984d96c4b986b488c5ee3492e882d8484f
Matt Saqueli <msaqueli22@turing.edu>
2023-01-11 15:24:25 -0600

    Fix the errors

---
+++ variables.js
@@ -0,0 +1,15 @@
+var email = "msaqueli@gmail.com"
+var name = "Matt Saqueli"
+
+var age = 26
+var turingCohort = 2301
+
+var frontendStudent = true
+var backendStudent = false
+
+email = "helloworld@gmail.com"
+name = "Mattheus"
+
+console.log(turingCohort = 2301)
+console.log(frontendStudent)
+console.log(backendStudent)
